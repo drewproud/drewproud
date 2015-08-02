dff41c48dae0db70bd66d1fedaab0429a7ee4548
Letladi <letladi@letladi.com>
2015-08-02 15:42:02 +0000

    Sorted out portfolio array

--- client/helpers.js
+++ client/helpers.js
@@ -1,5 +1,26 @@
+var portfolio = [
+  {
+    name: 'Thabang Tlaka', url: 'thabangtlaka.co.za', tools: ['html5 theme']
+  },
+  {
+    name: 'Corporate Youth', url: 'corporateyouth.co.za', tools: ['html5 theme']
+  }, 
+  {
+    name: 'Letladi', url: 'letladi.com', tools: ['css3', 'html5', 'MeteorJS']
+  }, 
+  {
+    name: 'Revolver Music', url: 'revolvermusic.co.za', tools: ['css3', 'html5', 'MeteorJS']
+  },
+  {
+    name: 'Banele Khoza', url: 'banelekhoza.co.za', tools: ['css3', 'html5', 'MeteorJS']
+  }
+];
+
 Template.footer.helpers({
   'year': function() {
     return new Date().getFullYear()
-  }
+  },
+  'projects': portfolio
 });
+
+ 
