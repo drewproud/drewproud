439c0c4030180ca842139fd2e2fc585bf3a247f1
Drew Proud <dproud@factset.com>
2015-08-21 13:34:24 -0400

    Updated styling to work better on mobile

--- client/sendEmail.js
+++ client/sendEmail.js
@@ -10,7 +10,16 @@ function sendEmail() {
   var name = $('form [name=name]').val().trim();
   var email = $('form [name=email]').val().trim();
   var message = $('form [name=message]').val().trim();
-  Meteor.call('sendEmail', name, email, message);
+
+  Meteor.call('sendEmail', name, email, message, function(err, res) {
+    if (err) {
+      toastr.error('Email failed to send. Please contact therealdrewproud@gmail.com');
+    } else {
+      toastr.success('Email Received. Thank You. I will get back to you in 24 Hours...');
+    }
+  });
+
+  $('form [name=name]').val('');
+  $('form [name=email]').val('');
   $('form [name=message]').val('');
-  toastr.success('Email Received. Thank You. I will get back to you in 24 Hours...');
 }
--- server/sendMail.js
+++ server/sendMail.js
@@ -1,17 +1,14 @@
-process.env.MAIL_URL = Meteor.settings.private.emailSetting; 
+process.env.MAIL_URL = Meteor.settings.private.emailSetting;
 
 Meteor.methods({
   sendEmail: function(name, fromEmail, message) {
     check([name, fromEmail, message], [String]);
-    
-    Meteor.defer(function() {
-      Email.send({
-        to: Meteor.settings.private.myEmailAddress,
-        from: fromEmail,
-        subject: Meteor.settings.private.subject + name,
-        text: message
-      });    
+
+    Email.send({
+      to: Meteor.settings.private.myEmailAddress,
+      from: fromEmail,
+      subject: Meteor.settings.private.subject + name,
+      text: message + '\n\n' + fromEmail
     });
   }
-  
 });
